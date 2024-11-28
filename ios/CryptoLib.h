7ce08d19e37c98f36ce657bbb6a858df73ddc2d2
prih <prih@prihhost.com>
2024-11-28 14:58:07 +0000

    fix: ios build

--- ios/CryptoLib.h
+++ ios/CryptoLib.h
@@ -2,8 +2,14 @@
 #import "react-native-crypto-lib.h"
 #endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNCryptoLibSpec.h"
+
+@interface CryptoLib : NSObject <NativeCryptoLibSpec>
+#else
 #import <React/RCTBridgeModule.h>
 
 @interface CryptoLib : NSObject <RCTBridgeModule>
+#endif
 
 @end
